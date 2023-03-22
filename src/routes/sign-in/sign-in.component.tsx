0c81c6dd92547c2556673e237f857a4956bbaef7
kishky101 <kishky.101@gmail.com>
2023-03-22 13:40:40 +0100

    added sign-in page for desktop and mobile

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,23 +1,69 @@
 import React from "react";
-import FormInput from "@/global-components/form-input/form-input.component";
-import Button from "@/global-components/button/button.component";
 import Logo from "@/global-components/logo/logo.component";
-import SelectInput from "@/global-components/select-input/select-input.component";
+import Button from "@/global-components/button/button.component";
+import FormInput from "@/global-components/form-input/form-input.component";
 
 import './sign-in.styles.scss';
 
 const SignIn: React.FC = () => {
-    return (
-        <>
-            <h1>I am Sign In Page</h1>
-            <FormInput type= {'radio'} />
-            <Button />
-            <Logo />
-            <SelectInput placeHolder="" options={['boy', 'girl', 'prefer not to say', 'house', 'child', '1', '2', '3', '4', '5', '6', '7']} onChange={(e) => console.log(e.target.name)} />
-            <div style={{height: '500px', border: '1px solid red'}}></div>
-        </>
-      
-    )
+
+  return (
+    <div className="sign-in">
+      <div className="sign-in__container">
+        <header className="sign-in__logo">
+          <Logo />
+        </header>
+        <h3 className="sign-in__h3">Please sign in to Spotify to continue.</h3>
+        <div className="sign-in__buttons">
+          <Button buttonType="facebook" ><img src="/src/assets/images/facebook.svg" /> Sign up with Facebook</Button>
+          <Button buttonType="apple" ><img src="/src/assets/images/apple.svg" /> Sign up with Apple</Button>
+          <Button ><img src="/src/assets/images/google.svg" /> Sign up with Google</Button>
+        </div>
+        <div className="sign-in__dividers">
+          <span className="sign-in__divider"></span> or <span className="sign-up__divider"></span>
+        </div>
+        <div className="sign-in__form">
+          <form className="sign-in__form-fields">
+            <div className="sign-in__text-input">
+              <FormInput 
+                type="email"
+                label="Email address or username"
+                placeholder="Email address or username"
+                htmlFor="email"
+              />
+              <FormInput 
+                type="password"
+                label="Password"
+                placeholder="Password"
+                htmlFor="password"
+              />
+              <span className="sign-in__profile">Do you not remember the password?</span>
+            </div>
+
+            <div className="sign-in__marketing">
+
+            </div>
+            <div className="sign-in__button">
+              <div className="sign-in__check">
+                <FormInput 
+                  type="checkbox"
+                  label="Remember me"
+                  htmlFor="marketing"
+                />
+              </div>
+              <Button buttonType="sign-in">Sign up</Button>
+            </div>
+          </form>
+          <span className="sign-in__divider2"></span>
+          <div className="sign-in__redirect">
+            <p>You do not have an account yet?</p>
+            <Button><span className="sign-in__redirect--opacity">Sign up for spotify</span></Button>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default SignIn;
